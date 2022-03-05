3c87c6b830398a78ebd8c5985a87bb5456767616
Essohanam <essoh.alou@gmail.com>
2022-03-05 11:42:43 +0000

    exercice helloWorld and repeatString successfuly solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(stringTomultiply, numberOfTimes) {
+    let resultToReturn = '';
+    if (numberOfTimes < 0) {
+        resultToReturn = 'ERROR';
+    } else if (numberOfTimes == 0) {
+        resultToReturn = '';
+    } else {
+
+        for (let i = 1; i <= numberOfTimes; i++) {
+            resultToReturn += `${stringTomultiply}`;
+
+        }
+    }
+    return resultToReturn;
 
 };
-
+//console.log(repeatString('me', 3));
 // Do not edit below this line
 module.exports = repeatString;
