8ea66bac300f414e4f23b3aca5d5e4a0eb54e23f
Essohanam <essoh.alou@gmail.com>
2022-03-07 22:35:52 +0000

    Exercices 5 and 6 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sumToreturn = 0;
+    if ((typeof(firstNumber) != 'number') || (typeof(secondNumber) != 'number') || (firstNumber < 0) || (secondNumber < 0)) {
+        sumToreturn = 'ERROR';
+    } else {
+        let maxNumber = Math.max(firstNumber, secondNumber);
+        let minNumber = Math.min(firstNumber, secondNumber);
+        let numberOfIterations = maxNumber - minNumber;
+        let numberIterated = minNumber;
+        sumToreturn = minNumber;
+        for (let i = 0; i < numberOfIterations; i++) {
+            numberIterated += 1;
+            sumToreturn += numberIterated;
+        }
+    }
+    return sumToreturn;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
