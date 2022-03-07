843d8fd7d433783a313b69a1ffc065a940fa404e
Essohanam <essoh.alou@gmail.com>
2022-03-07 21:02:25 +0000

    first 4 exercices solved successfuly

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(textToreversre) {
+    let stringLength = textToreversre.length;
+    let stringReversed = '';
+    if (stringLength == 0) {
+        stringReversed = '';
+    } else {
+        for (let i = 1; i <= stringLength; i++) {
+            stringReversed += textToreversre[stringLength - i];
+
+        }
+    }
+    return stringReversed;
 
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
 const removeFromArray = function() {
+    let arrayToreturn = [];
+    let arrayFromWichToRemove = arguments[0];
+    let argumentsToTestRemoveFromArray = [];
+    for (let i = 1; i < arguments.length; i++) {
+        argumentsToTestRemoveFromArray.push(arguments[i]);
+    }
+    for (item of arrayFromWichToRemove) {
+        let testResult = false;
+        for (element of argumentsToTestRemoveFromArray) {
 
+            if (item === element) {
+                testResult = true
+
+            }
+        }
+        if (!testResult) {
+            arrayToreturn.push(item);
+        }
+
+    }
+    return arrayToreturn;
 };
 
 // Do not edit below this line
