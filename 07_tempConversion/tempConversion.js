9ebd3fdf44cdb58dc6100aa488a662d6cdbc9416
Essohanam <essoh.alou@gmail.com>
2022-03-08 05:12:38 +0000

    exercices 7 and 8 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
+const ftoc = function(degreeF) {
+    let degreeC;
+    degreeC = Math.round(((degreeF - 32) * (5 / 9)) * 10) / 10;
+    return degreeC;
 
 };
 
-const ctof = function() {
+const ctof = function(degreeC) {
+    let degreeF;
+    degreeF = Math.round((degreeC * (9 / 5) + 32) * 10) / 10;
+    return degreeF;
 
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,63 @@
 const add = function() {
-	
+    let length = arguments.length;
+    let argumentsSum = 0;
+    for (let i = 0; i < length; i++) {
+        argumentsSum += arguments[i];
+    }
+    return argumentsSum;
 };
 
 const subtract = function() {
-	
+    return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+    let elementsSum = 0;
+    let arrayIputed = arguments[0]
+    for (let i = 0; i < arrayIputed.length; i++) {
+        elementsSum += arrayIputed[i];
+    }
+    return elementsSum;
 };
 
 const multiply = function() {
-
+    let arrayInputed = arguments[0];
+    let elementsMultiplicated = 1;
+    for (let i = 0; i < arrayInputed.length; i++) {
+        elementsMultiplicated *= arrayInputed[i];
+    }
+    return elementsMultiplicated;
 };
 
 const power = function() {
-	
+    let basisNumber = arguments[0];
+    let powerNumber = arguments[1];
+    return basisNumber ** powerNumber;
+
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(positiveInteger) {
+    let factorialOfTheNumber = 1;
+    if (positiveInteger === 0 || positiveInteger === 1) {
+        factorialOfTheNumber = 1
+        return factorialOfTheNumber;
+    } else {
 
+        while (positiveInteger > 1) {
+            factorialOfTheNumber *= positiveInteger;
+            positiveInteger = positiveInteger - 1;
+
+        }
+    }
+    return factorialOfTheNumber;
+};
+console.log(multiply([2, 3, 2]));
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
