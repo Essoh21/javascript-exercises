1962b3719e2edc9407b00714a7ef342fc9a3f46d
Essohanam <essoh.alou@gmail.com>
2022-04-25 07:55:54 +0000

    findTheOdest solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfObjects) {
+    const theOldest = arrayOfObjects.reduce((previousObject, currentObject) => {
+        let year = new Date();
+        if (!('yearOfDeath' in currentObject)) {
 
+            currentObject['yearOfDeath'] = year.getFullYear();
+        }
+        if (!('yearOfDeath' in previousObject)) {
+            previousObject['yearOfDeath'] = year.getFullYear();
+        }
+        if ((currentObject.yearOfDeath - currentObject.yearOfBirth) > (previousObject.yearOfDeath - previousObject.yearOfBirth)) {
+            return currentObject;
+        } else {
+            return previousObject;
+        }
+    })
+    return theOldest;
 };
 
 // Do not edit below this line
